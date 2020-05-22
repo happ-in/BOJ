f18ca0e4ec2aa2f4cd62c90427a2c6c4f2a19101
hyebinppark <gpqls304@naver.com>
2020-05-22 22:08:03 +0900

    11726번: 2xn 타일링

---
+++ N11726.java
@@ -0,0 +1,16 @@
+package com.company.bakjoon;
+
+import java.util.*;
+
+public class N11726 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        long[] dp = new long[n+1];
+        dp[0]=1; dp[1]=1;
+        for(int i=2; i<=n; i++) dp[i] = (dp[i-1] + dp[i-2]%10007);
+
+        System.out.print(dp[n]);
+    }
+}
