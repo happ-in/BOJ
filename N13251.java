01b4235e8ffa4f35e06ebf7dfe71c838820da99a
hyebinppark <gpqls304@naver.com>
2020-08-15 19:08:53 +0900

    13251번: 조약돌 꺼내기

---
+++ N13251.java
@@ -0,0 +1,33 @@
+package com.company.bakjoon;
+
+import java.util.*;
+
+public class N13251 {
+    static int M, K;
+    static int[] arr;
+
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        M = sc.nextInt();
+
+        arr = new int[M];
+        int total = 0;
+        for(int i=0; i<M; i++) { arr[i] = sc.nextInt(); total += arr[i];}
+
+        K = sc.nextInt();
+
+        double bottom = 1;
+        for(int i=total; i>total-K; i--) bottom = bottom * i;
+
+        double top = 0;
+        for(int i=0; i<M; i++){
+            if(arr[i]<K) continue;
+            double tmp = 1;
+            for(int j=arr[i]; j>arr[i]-K; j--) tmp *= j;
+
+            top += tmp;
+        }
+
+        System.out.print(top/bottom);
+    }
+}
