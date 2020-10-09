3a08c62135748f9d2bebb1b8c569b7f867e2bb6f
hyebinppark <gpqls304@naver.com>
2020-10-10 00:06:27 +0900

    2292번: 벌집

---
+++ N2292.java
@@ -0,0 +1,26 @@
+package com.company.bakjoon;
+
+import java.util.*;
+
+public class N2292 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int answer = 1;
+        long val = 1; int depth = 1;
+        long start = 1; long end = 1;
+        while(true) {
+            if(N>=start && N<=end) {
+                System.out.print(answer);
+                return;
+            }
+
+            val = 6*depth;
+            start = end+1;
+            end += val;
+
+            depth++; answer++;
+        }
+    }
+}
