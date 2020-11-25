746a39efc3a0b1d8aeab67f46b091aad3124fb21
hyebinppark <gpqls304@naver.com>
2020-11-25 13:47:04 +0900

    18512번: 점프 점프

---
+++ N18512.java
@@ -0,0 +1,32 @@
+package com.company.bakjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class N18512 {
+
+    public static void main(String[] args) throws Exception{
+        System.setIn(new FileInputStream("src/com/company/test.txt"));
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Scanner sc = new Scanner(System.in);
+
+        int jump_A = sc.nextInt();
+        int jump_B = sc.nextInt();
+        int pos_A = sc.nextInt();
+        int pos_B = sc.nextInt();
+
+        while(pos_A <= 1000000) {
+            if(pos_A == pos_B) {
+                System.out.print(pos_A);
+                return;
+            } else if(pos_A < pos_B) {
+                pos_A += jump_A;
+            } else {
+                pos_B += jump_B;
+            }
+        }
+
+        System.out.print("-1");
+    }
+}
